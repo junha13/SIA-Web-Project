d1d88a2280df53dba1682554e25ed391992679b9
junha13 <tnznf1113@gmail.com>
2025-08-17 18:22:47 +0900

    Add files via upload

---
+++ final.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE IF NOT EXISTS trablog; 
+use trablog;
+
+CREATE TABLE tb_user ( #user는 안된대 
+pkid int auto_increment primary key, 
+loginId VARCHAR(50) NOT NULL UNIQUE, 
+loginPw VARCHAR(100) NOT NULL, 
+loginName VARCHAR(50) NOT NULL, 
+phoneNumber VARCHAR(20), 
+email VARCHAR(100), 
+birth varchar(100) 
+);
+
+DROP TABLE tb_user;
+
+ select * from tb_user;
+ 
+ CREATE TABLE tb_post ( #user는 안된대 
+pkid int auto_increment primary key, 
+postTitle VARCHAR(50) NOT NULL, 
+postLocation VARCHAR(50) NOT NULL,
+postContent mediumtext NOT NULL
+);
+
+ALTER TABLE tb_post ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+DROP TABLE tb_post;
+
+ select * from tb_post;
+ 
+ create table tb_regions (
+ sggCode varchar(10) not null PRIMARY KEY,
+ sidoName varchar(20) not null,
+ sggName varchar(20) not null,
+ label varchar(30) not null 
+ );
+ 
+ DROP TABLE tb_regions;
+
+ select * from tb_regions;
+ 
+ 
+ #더미 인서트 데이터 // gpt한테 이렇게 인서트데이터 써달라고 하면 200몇갠가 다 써주긴함 (확인필수)
+ INSERT INTO tb_regions (sggCode, sidoName, sggName, label) VALUES
+('11020', '서울특별시', '구로구', '서울특별시 구로구'),
+('11010', '서울특별시', '강남구', '서울특별시 강남구'),
+('11011', '대구광역시', '강남구', '대구광역시 강남구'),
+('31020', '경기도', '수원시', '경기도 수원시'),
+('39010', '제주특별자치도', '제주시', '제주특별자치도 제주시'),
+('39020', '제주특별자치도', '서귀포시', '제주특별자치도 서귀포시');
+ 
